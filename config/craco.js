b7b21f6548b596aea8412321d924072f1a480497
Crazy Urus <crazyurus@vip.qq.com>
2023-03-08 19:29:03 +0800

    feat: 接入 craco 插件

---
+++ config/craco.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
+const CracoCSSModulesPlugin = require('craco-css-modules');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
+const isProduction = process.env.NODE_ENV === 'production';
+const isAnalysis = process.env.ANALYZE === 'TRUE';
+
+process.env.BUILD_PATH = path.resolve('./dist');
+process.env.GENERATE_SOURCEMAP = !isProduction.toString();
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+    {
+      plugin: CracoCSSModulesPlugin,
+    },
+  ],
+  style: {
+    modules: {
+      localIdentName: '[local]--[hash:base64:5]',
+      exportLocalsConvention: 'camelCase',
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+    plugins: {
+      add: [
+        new SubresourceIntegrityPlugin({
+          hashFuncNames: ['sha256', 'sha512'],
+          enabled: isProduction,
+        }),
+      ].concat(
+        isAnalysis ? [new BundleAnalyzerPlugin(), new SpeedMeasurePlugin()] : []
+      ),
+      remove: ['WebpackManifestPlugin', 'ESLintWebpackPlugin'],
+    },
+    configure(options) {
+      options.output.crossOriginLoading = 'anonymous';
+      options.devtool = isProduction ? 'hidden-source-map' : 'eval';
+      options.watchOptions = {
+        ignored: /node_modules/,
+      };
+      options.resolve.extensions = ['.js', '.ts', '.tsx'];
+
+      return options;
+    },
+  },
+};
